b81368cb0d9521fa407e02f616d7f2533b71f865
Alexandre Souza <alexandresouza9@gmail.com>
2023-07-31 12:03:26 -0300

    commit

--- hotel.py
+++ hotel.py
@@ -1,8 +1,15 @@
-andar=0
+#Precisamos imprimir um número para cada andar de um hotel de 20 andares. Porém, o dono do hotel é supersticioso e optou por não ter um 13ro andar.
+#Escreva um código que imprima todos os números exceto o número 13.
+#Escreva mais dois códigos que resolvam o mesmo problema, mas dessa vez usando os outros dois tipos de laços de repetição aprendidos.
+#Como desafio, imprima eles em ordem decrescente (20, 19, 18...)
 
+andar=0
+#primeiro código
 if andar==0:
   print('Térreo')
 for andar in range(1, 13):
   print(andar,'º Andar')
-for andar in range(14, 21):
+# segundo código
+print("ordem decrescente")
+for andar in range(20, 0, -1):
   print(andar,'º Andar')
