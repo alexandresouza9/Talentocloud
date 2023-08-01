7d22d2b600e9f7285d380a3cf5bd90bde106608c
Alexandre Souza <alexandresouza9@gmail.com>
2023-08-01 11:50:25 -0300

    commit

--- Exercicios/calculadora.py
+++ Exercicios/calculadora.py
@@ -1,3 +1,10 @@
+#Faça uma função calculadora de dois números com três parâmetros: os dois primeiros serão os números da operação e o terceiro será a entrada que definirá 
+# a operação a ser executada. Considera a seguinte definição:
+# 1. Soma
+# 2. Subtração
+# 3. Multiplicação
+# 4. Divisão
+
 print("Digite um numero1")
 numero1 = int(input())
 
