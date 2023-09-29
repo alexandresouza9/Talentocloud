fa739d68953290f77e79179d511cc4508d4f7e1c
alexandresouza9 <alexandresouza9@gmail.com>
2023-09-29 17:20:16 -0300

    commit
    commit

--- JavaScript I/script.js
+++ JavaScript I/script.js
@@ -1,14 +1,7 @@
-const texto = "Conexão com um arquivo JavaScript"
-
-
-for(let i=0; i<3; i++){
-    // Criar o elemento
-    const h2 = document.createElement("h2")
-
-    //Popular esse elemento
-    h2.innerText = "Conexão com um arquivo JavaScript"
-
-    // Adicionar elemento na DOM
-    const body = document.querySelector("body")
-    body.appendChild(h2)
-}
+function imprimirNoTerminal() {
+    console.log("Conexão feita com sucesso!");
+  }
+  
+  imprimirNoTerminal();
+  imprimirNoTerminal();
+  imprimirNoTerminal();
