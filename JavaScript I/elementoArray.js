cf284ffca8a33fa5ffe61b8b771b6d51315be2d9
alexandresouza9 <alexandresouza9@gmail.com>
2023-10-05 20:03:30 -0300

    commit

--- JavaScript I/elementoArray.js
+++ JavaScript I/elementoArray.js
@@ -4,6 +4,6 @@ const paresMenorQue50 = numerosDaSorte.filter(numero => numero % 2 === 0 && nume
 const menorQue50 = numerosDaSorte.filter(numero => numero < 50);
 const maiorQue50 = numerosDaSorte.filter(numero => numero > 50);
 
-console.log('Números pares e menores que 50:', paresMenorQue50);
-console.log('Números menores que 50:', menorQue50);
-console.log('Números maiores que 50:', maiorQue50);
+console.log("Números pares e menores que 50:" , paresMenorQue50);
+console.log("Números menores que 50:" , menorQue50);
+console.log("Números maiores que 50:" , maiorQue50);
