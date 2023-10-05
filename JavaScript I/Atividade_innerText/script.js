40f4ed58118a0367c268739a8779ad541772d467
alexandresouza9 <alexandresouza9@gmail.com>
2023-10-05 20:16:20 -0300

    commit

--- JavaScript I/Atividade_innerText/script.js
+++ JavaScript I/Atividade_innerText/script.js
@@ -1,19 +1,24 @@
-let titulo = document.getElementById("titulo")
-let ulElement = document.querySelector("ul")
-let aElement = document.querySelector("a")
-let olElement = document.getElementById("lista-ordenada")
+// Capturando os elementos h1 e a
+let titulo = document.getElementById('titulo')
+let link = document.querySelector('a');
 
-titulo.innerText = "Título da Página"
-aElement.innerText = "Visite o site da Prozeducação"
+// Adicionando conteúdo textual neles
+titulo.innerText = 'Temas estudados'
+link.innerText = 'Site da Proz'
 
-ulElement.innerHTML = `
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
+// Capturando os elementos ul e ol
+let listaNaoOrdenada = document.querySelector('ul')
+let listaOrdenada = document.querySelector('ol')
+
+// Adicionando três itens simples na lista não ordenada
+listaNaoOrdenada.innerHTML = `
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
 `
-
-olElement.innerHTML = `
-    <li><a href="https://exemplo1.com">Link 1</a></li>
-    <li><a href="https://exemplo2.com">Link 2</a></li>
-    <li><a href="https://exemplo3.com">Link 3</a></li>
+// Adicionando três itens com links na lista ordenada
+listaOrdenada.innerHTML = `
+  <li><a href="https://about.meta.com/">Meta</a></li>
+  <li><a href="https://www.aws.com">AWS</a></li>
+  <li><a href="https://www.google.com">Google</a></li>
 `
